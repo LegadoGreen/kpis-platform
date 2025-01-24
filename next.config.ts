d6080d28f4af8efe74064a22870a340268b477ab
David Vargas <davids10.dvm@gmail.com>
2025-01-24 10:46:53 -0500

    Fix: solved problem with base path on images

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY
-  }
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
 };
 
 export default nextConfig;
